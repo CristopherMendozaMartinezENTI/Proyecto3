09ce4142410f19a363d21d3d0a7c89fdcf63a694
TheKrizXP1 <thekrisxp1@gmail.com>
2021-09-23 18:53:32 +0200

    Fist Commit - Technical Demo - Procedural Spider

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 3f;
+    private float smoothness = 5f;
+    private int raysNb = 8;
+    private float raysEccentricity = 0.2f;
+    private float outerRaysOffset = 2f;
+    private float innerRaysOffset = 25f;
+    private Vector3 velocity;
+    private Vector3 lastVelocity;
+    private Vector3 lastPosition;
+    private Vector3 forward;
+    private Vector3 upward;
+    private Quaternion lastRot;     
+    private Vector3[] pn;
+
+    private void Start()
+    {
+        velocity = new Vector3();
+        forward = transform.forward;
+        upward = transform.up;
+        lastRot = transform.rotation;
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = (smoothness * velocity + (transform.position - lastPosition)) / (1f + smoothness);
+        if (velocity.magnitude < 0.00025f)
+            velocity = lastVelocity;
+        lastPosition = transform.position;
+        lastVelocity = velocity;
+     
+        //Run
+        float multiplier = 1f;
+        if (Input.GetKey(KeyCode.LeftShift))
+            multiplier = 2f;
+
+        //Move Foward
+        bool moveFoward = Input.GetKey(KeyCode.W);
+        if (moveFoward != false)
+            transform.position += transform.forward * speed * multiplier * Time.fixedDeltaTime;
+
+        //Move Backward
+        bool moveBackward = Input.GetKey(KeyCode.S);
+        if (moveBackward != false)
+            transform.position += -transform.forward * speed * multiplier * Time.fixedDeltaTime;
+
+        //Look left or Right     
+        float valueX = Input.GetAxis("Horizontal");
+        if (valueX != 0)
+           transform.position += Vector3.Cross(transform.up, transform.forward) * valueX * speed * multiplier * Time.fixedDeltaTime;
+
+        if (moveFoward != false || moveBackward != false || valueX != 0)
+        {
+            pn = GetClosestPoint(transform.position, transform.forward, transform.up, 0.5f, 0.1f, 30, -30, 4);
+            upward = pn[1];
+            Vector3[] pos = GetClosestPoint(transform.position, transform.forward, transform.up, 0.5f, raysEccentricity, innerRaysOffset, outerRaysOffset, raysNb);
+            transform.position = Vector3.Lerp(lastPosition, pos[0], 1f / (1f + smoothness));
+            forward = velocity.normalized;
+            Quaternion q = Quaternion.LookRotation(forward, upward);
+            transform.rotation = Quaternion.Lerp(lastRot, q, 1f / (1f + smoothness));
+        }
+        lastRot = transform.rotation;
+    }
+
+    Vector3[] GetIcoSphereCoords(int depth)
+    {
+        Vector3[] res = new Vector3[(int)Mathf.Pow(4, depth) * 12];
+        float t = (1f + Mathf.Sqrt(5f)) / 2f;
+        res[0] = (new Vector3(t, 1, 0));
+        res[1] = (new Vector3(-t, -1, 0));
+        res[2] = (new Vector3(-1, 0, t));
+        res[3] = (new Vector3(0, -t, 1));
+        res[4] = (new Vector3(-t, 1, 0));
+        res[5] = (new Vector3(1, 0, t));
+        res[6] = (new Vector3(-1, 0, -t));
+        res[7] = (new Vector3(0, t, -1));
+        res[8] = (new Vector3(t, -1, 0));
+        res[9] = (new Vector3(1, 0, -t));
+        res[10] = (new Vector3(0, t, 1));
+        res[11] = (new Vector3(0, -t, -1));
+        return res;
+    }
+
+    Vector3[] GetClosestPointIco(Vector3 point, Vector3 up, float halfRange)
+    {
+        Vector3[] res = new Vector3[2] { point, up };
+
+        Vector3[] dirs = GetIcoSphereCoords(0);
+        raysNb = dirs.Length;
+
+        float amount = 1f;
+
+        foreach (Vector3 dir in dirs)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(point + up * 0.15f, dir);
+            if (Physics.SphereCast(ray, 0.01f, out hit, 2f * halfRange))
+            {
+                res[0] += hit.point;
+                res[1] += hit.normal;
+                amount += 1;
+            }
+        }
+        res[0] /= amount;
+        res[1] /= amount;
+        return res;
+    }
+
+    static Vector3[] GetClosestPoint(Vector3 point, Vector3 forward, Vector3 up, float halfRange, float eccentricity, float offset1, float offset2, int rayAmount)
+    {
+        Vector3[] res = new Vector3[2] { point, up };
+        Vector3 right = Vector3.Cross(up, forward);
+        float normalAmount = 1f;
+        float positionAmount = 1f;
+
+        Vector3[] dirs = new Vector3[rayAmount];
+        float angularStep = 2f * Mathf.PI / (float)rayAmount;
+        float currentAngle = angularStep / 2f;
+        for (int i = 0; i < rayAmount; ++i)
+        {
+            dirs[i] = -up + (right * Mathf.Cos(currentAngle) + forward * Mathf.Sin(currentAngle)) * eccentricity;
+            currentAngle += angularStep;
+        }
+
+        foreach (Vector3 dir in dirs)
+        {
+            RaycastHit hit;
+            Vector3 largener = Vector3.ProjectOnPlane(dir, up);
+            Ray ray = new Ray(point - (dir + largener) * halfRange + largener.normalized * offset1 / 100f, dir);
+
+            if (Physics.SphereCast(ray, 0.01f, out hit, 2f * halfRange))
+            {
+                res[0] += hit.point;
+                res[1] += hit.normal;
+                normalAmount += 1;
+                positionAmount += 1;
+            }
+            ray = new Ray(point - (dir + largener) * halfRange + largener.normalized * offset2 / 100f, dir);
+            if (Physics.SphereCast(ray, 0.01f, out hit, 2f * halfRange))
+            {
+                res[0] += hit.point;
+                res[1] += hit.normal;
+                normalAmount += 1;
+                positionAmount += 1;
+            }
+        }
+        res[0] /= positionAmount;
+        res[1] /= normalAmount;
+        return res;
+    }
+}
