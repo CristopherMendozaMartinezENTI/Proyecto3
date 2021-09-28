234bed48b05c7da4a8248a06ebf6e29183e72bed
TheKrizXP1 <thekrisxp1@gmail.com>
2021-09-29 00:37:28 +0200

    Prototype Build Ready

--- Assets/Scripts/RotateIcon.cs
+++ Assets/Scripts/RotateIcon.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class RotateIcon : MonoBehaviour
 {
+    public bool rotate;
+    public bool resize;
+    public float rotationSpeed;
+    private float realRotation;
+    public float minScale;
+    public float maxScale;
+    private Vector3 scaleChange;
 
-    public float speed;
-    float realRotation = 0;
+    private void Start()
+    {
+        realRotation = 0;
+        scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+    }
 
     private void Update()
     {
-        realRotation++;
+        if (rotate)
+        {
+            realRotation++;
 
-        if (realRotation == 360)
-            realRotation = 0;
+            if (realRotation == 360)
+                realRotation = 0;
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -realRotation, 0), speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -realRotation, 0), rotationSpeed);
+        }
+
+        if(resize)
+        {
+            transform.localScale += scaleChange;
+            if (transform.localScale.y < minScale || transform.localScale.y > maxScale)
+            {
+                scaleChange = -scaleChange;
+            }
+        }
     }
 }
