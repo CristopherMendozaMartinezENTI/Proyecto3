323109caa10932a120a5e2b4f2a57c9cec56d4ca
TheKrizXP1 <thekrisxp1@gmail.com>
2021-10-13 17:07:06 +0200

    New Web System. Now U can choose Normal or Gravitional web.

--- Assets/Scripts/Player/Bit/HookSystem.cs
+++ Assets/Scripts/Player/Bit/HookSystem.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WebTType { Normal, Gravitational }
+
 public class HookSystem : MonoBehaviour
 {
     [Header("Options")]
     [SerializeField] private float maxGrabDistance = 3.0f;
+    [SerializeField] private WebTType _webType;
+    [SerializeField] private GameObject legRig;
     private float currentGrabDistance;
     private Vector2 rotationInput;
     private Vector3 hitOffsetLocal;
@@ -13,10 +17,12 @@ public class HookSystem : MonoBehaviour
     private RigidbodyInterpolation initialInterpolationSetting;
     private GameObject player;
     private Rigidbody grabbedRigidbody;
+ 
 
     private void Start()
     {
         player = GameObject.Find("Player");
+        _webType = WebTType.Normal;
     }
 
     private void FixedUpdate()
@@ -24,31 +30,58 @@ public class HookSystem : MonoBehaviour
         // We are holding an object
         if (grabbedRigidbody)
         {
-            //We place the object in the space in relation with the camera position 
-            Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
-            grabbedRigidbody.MoveRotation(Quaternion.Euler(rotationDifferenceEuler + transform.rotation.eulerAngles));
-            Vector3 holdPoint = ray.GetPoint(currentGrabDistance);
-            Vector3 currentEuler = grabbedRigidbody.rotation.eulerAngles;
-            grabbedRigidbody.transform.RotateAround(holdPoint, transform.right, rotationInput.y);
-            grabbedRigidbody.transform.RotateAround(holdPoint, transform.up, -rotationInput.x);
-            grabbedRigidbody.angularVelocity = Vector3.zero;
-            rotationInput = Vector2.zero;
-            rotationDifferenceEuler = grabbedRigidbody.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
-            Vector3 centerDestination = holdPoint - grabbedRigidbody.transform.TransformVector(hitOffsetLocal);
-            Vector3 toDestination = centerDestination - grabbedRigidbody.transform.position;
-            Vector3 force = toDestination / Time.fixedDeltaTime;
-            grabbedRigidbody.velocity = Vector3.zero;
-            grabbedRigidbody.AddForce(force, ForceMode.VelocityChange);
-
-            if (Input.GetKeyDown(KeyCode.LeftControl))
+            switch (_webType)
             {
-                grabbedRigidbody.isKinematic = true;
+                case WebTType.Normal:
+                    //We Move the object towards the player
+                    player.GetComponent<PlayerController>().enabled = false;
+                    grabbedRigidbody.transform.position = Vector3.MoveTowards(grabbedRigidbody.transform.position, player.transform.position, 0.5f * Time.fixedDeltaTime);
+                    if (Vector3.Distance(grabbedRigidbody.transform.position, player.transform.position) < 0.1f)
+                    {
+                        grabbedRigidbody.transform.position *= -1.0f;
+                    }
+                    //We mode the rig towards his flex position
+                    if (legRig.transform.localPosition.z != -0.5) legRig.transform.localPosition = Vector3.MoveTowards(legRig.transform.localPosition, new Vector3(0, 0, -0.5f), 0.2f * Time.fixedDeltaTime);
+                    return;
+                case WebTType.Gravitational:
+                    //We place the object in the space in relation with the camera position 
+                    Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
+                    grabbedRigidbody.MoveRotation(Quaternion.Euler(rotationDifferenceEuler + transform.rotation.eulerAngles));
+                    Vector3 holdPoint = ray.GetPoint(currentGrabDistance);
+                    Vector3 currentEuler = grabbedRigidbody.rotation.eulerAngles;
+                    grabbedRigidbody.transform.RotateAround(holdPoint, transform.right, rotationInput.y);
+                    grabbedRigidbody.transform.RotateAround(holdPoint, transform.up, -rotationInput.x);
+                    grabbedRigidbody.angularVelocity = Vector3.zero;
+                    rotationInput = Vector2.zero;
+                    rotationDifferenceEuler = grabbedRigidbody.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
+                    Vector3 centerDestination = holdPoint - grabbedRigidbody.transform.TransformVector(hitOffsetLocal);
+                    Vector3 toDestination = centerDestination - grabbedRigidbody.transform.position;
+                    Vector3 force = toDestination / Time.fixedDeltaTime;
+                    grabbedRigidbody.velocity = Vector3.zero;
+                    grabbedRigidbody.AddForce(force, ForceMode.VelocityChange);
+                    if (Input.GetKeyDown(KeyCode.LeftControl))
+                    {
+                        grabbedRigidbody.isKinematic = true;
+                    }
+                    return;
             }
         }
+        //We mode the rig towards his neutral point
+        if (legRig.transform.localPosition.z >= -0.5)  legRig.transform.localPosition = Vector3.MoveTowards(legRig.transform.localPosition, new Vector3(0, 0, 0), 0.5f * Time.fixedDeltaTime);
     }
 
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _webType = WebTType.Normal;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _webType = WebTType.Gravitational;
+        }
+
         //Here we are hovering on posible targets
         Ray rayHover = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
         RaycastHit hitHover;
@@ -65,6 +98,7 @@ public class HookSystem : MonoBehaviour
             // Reset the rigidbody to how it was before we grabbed it
             if (grabbedRigidbody != null)
             {
+                player.GetComponent<PlayerController>().enabled = true;
                 grabbedRigidbody.transform.tag = "Obstacle";
                 grabbedRigidbody.interpolation = initialInterpolationSetting;
                 grabbedRigidbody.transform.parent = null;
